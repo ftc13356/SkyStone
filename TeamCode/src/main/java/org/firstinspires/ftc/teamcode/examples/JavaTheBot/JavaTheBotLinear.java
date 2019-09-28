812ab0c6cefba3e271ef8b2a98a01b8be1e36ef4
Jonathan Ma <jonathanjma03@gmail.com>
2019-09-27 22:06:35 -0700

    converted teleop opmode to linear opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/JavaTheBot/JavaTheBotLinear.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.examples.JavaTheBot;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="JavaTheBot")
+@Disabled
+public class JavaTheBotLinear extends LinearOpMode {
+    //Comment
+    DcMotor left;
+    DcMotor right;
+
+    @Override
+    public void runOpMode() {
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+
+        waitForStart();
+
+        double driveFW = gamepad1.left_stick_y;
+        double turn = gamepad1.right_stick_x;
+
+        double leftPower = driveFW - turn;
+        double rightPower = -driveFW - turn;
+
+        left.setPower(leftPower);
+        right.setPower(rightPower);
+    }
+}
