9d700e0aa9b370920ead333f0a4d1e4db6ba3b15
redpandasforlife <aidenjma@gmail.com>
2019-10-12 10:38:21 -0700

    no message

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/SkyStoneAutonomous_Aiden.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Skystone;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Skystone Demo")
+public class SkyStone_Autonomous_Aiden extends LinearOpMode {
+
+
+
+    @Override
+    public void runOpMode() {
+
+        basicChassis myChassis = new basicChassis();
+
+        waitForStart();
+
+        myChassis.moveForward(22);
+
+
+        myChassis.moveForward(5);
+        stop();
+
+    }
+}
