7de8e6c898b04a28a662e459cfd2242f1513d3e1
redpandasforlife <aidenjma@gmail.com>
2019-10-14 19:07:20 -0700

    Autonomous Path 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/SkyStoneAutonomous_Aiden_3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/SkyStoneAutonomous_Aiden_3.java
@@ -2,7 +2,8 @@ package org.firstinspires.ftc.teamcode.Skystone;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 /**
  * <h1>first SkyStone autonomous program</h1>
  * for testing basicChassis that Nathan and Andrew wrote
@@ -14,8 +15,15 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
  * @version 1.0
  * @since   2019-10-12
  */
-@Autonomous(name = "Skystone Demo")
-public class SkyStoneAutonomous_Aiden_3 extends basicChassis {
+@Autonomous(name = "Skystone Demo3")
+public class SkyStoneAutonomous_Aiden_3 extends LinearOpMode {
+
+    basicChassis         robot   = new basicChassis();
+    private ElapsedTime  runtime = new ElapsedTime();
+
+    public void SkyStoneAutonomous_Aiden_3(){
+
+    }
 
     /**
      * This method is for te autonomous operation of the robot on the Blue Alliance SkyStone side.
@@ -28,13 +36,20 @@ public class SkyStoneAutonomous_Aiden_3 extends basicChassis {
     @Override
     public void runOpMode() {
 
+        telemetry.addData("Status", "Ready to go"); telemetry.update();
+
+        robot.initChassis(this);
+
         waitForStart();
-        moveForward(42);
-        inPlaceTurn(130,true);
-        moveForward(46);
-        inPlaceTurn(40,false);
-        moveBackward(22);
+        robot.moveForward(28);
+        robot.moveBackward(5);
+        robot. inPlaceTurn(130,true);
+        robot.moveForward(46);
+        robot.inPlaceTurn(40,false);
+        robot.moveBackward(28);
+
         stop();
+
 
     }
 }
