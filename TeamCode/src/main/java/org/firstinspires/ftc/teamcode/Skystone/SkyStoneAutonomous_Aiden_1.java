192597387548c3039cd4c2562cc7ad375e772f8b
redpandasforlife <aidenjma@gmail.com>
2019-10-14 19:06:41 -0700

    Autonomous Path 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/SkyStoneAutonomous_Aiden_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/SkyStoneAutonomous_Aiden_1.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.Skystone;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 /**
  * <h1>first SkyStone autonomous program</h1>
@@ -14,9 +17,15 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
  * @version 1.0
  * @since   2019-10-12
  */
-@Autonomous(name = "Skystone Demo")
-public class SkyStoneAutonomous_Aiden_1 extends basicChassis {
+@Autonomous(name = "Skystone Demo1")
+public class SkyStoneAutonomous_Aiden_1 extends LinearOpMode {
 
+    basicChassis         robot   = new basicChassis();
+    private ElapsedTime  runtime = new ElapsedTime();
+
+    public void SkyStoneAutonomous_Aiden_1 () {
+
+    }
     /**
      * This method is for te autonomous operation of the robot on the foundation side.
      * 1st you inPlaceTurn left
@@ -31,18 +40,27 @@ public class SkyStoneAutonomous_Aiden_1 extends basicChassis {
     @Override
     public void runOpMode() {
 
+        telemetry.addData("Status", "Ready to go"); telemetry.update();
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.initChassis(this);
+
         waitForStart();
-        inPlaceTurn(85,true);
-        moveForward(18);
-        inPlaceTurn(85,false);
-        moveForward(24);
+        robot.moveForward(8);
+        robot.inPlaceTurn(130,true);
+        sleep((long) 100);
+        robot.moveForward(15);
+        robot.inPlaceTurn(115,false);
+        robot.moveForward(16);
         //need servo program to clamp on foundation
-        moveBackward(24);
+        robot.moveBackward(24);
         //need servo program to unclamp foundation
-        inPlaceTurn(85,true);
-        moveBackward(32);
+        robot.inPlaceTurn(90,true);
+        robot.moveBackward(70);
 
         stop();
-
     }
 }
