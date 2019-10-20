012edf061a7bf9e5559dfbd7cb2ed812467c965e
Andrew <andrewluzhao@gmail.com>
2019-10-20 12:09:52 -0700

    Fixed formatting of this file and fixed one syntax error, this code has not been tested(normally should not push code that has not been tested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/hexChassis_Andrew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/hexChassis_Andrew.java
@@ -7,14 +7,15 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.teamcode.examples.autonomous.autonomousFrame;
 
 
 /**
  * Author: Andrew
  * Change Proposal: Need permission by author to make changes. Slack me to discuss first
- *                  This is because your Changes could possible disrupt my other code unexpectedly
- *                  Thanks for your understanding
+ * This is because your Changes could possible disrupt my other code unexpectedly
+ * Thanks for your understanding
  */
 
 public abstract class hexChassis_Andrew extends LinearOpMode {
@@ -24,9 +25,9 @@ public abstract class hexChassis_Andrew extends LinearOpMode {
     public DcMotor rightFront;
     public DcMotor rightBack;
 
-    private LinearOpMode myOP             = null;
-    private HardwareMap myHaredwareMap     = null;
-    private ElapsedTime period          = new ElapsedTime();
+    private LinearOpMode myOP = null;
+    private HardwareMap myHaredwareMap = null;
+    private ElapsedTime period = new ElapsedTime();
 
     //private float speed = 37.5f;
     final static double ROBOT_DIAMETER = 14.0;
@@ -35,33 +36,34 @@ public abstract class hexChassis_Andrew extends LinearOpMode {
     final static float DEFAULT_POWER = 0.25f;
 
     public final static double TICK_MARKS = 288;
-    public final static double TICKS_PER_INCH = TICK_MARKS * CALLIBRATION/(WHEEL_DIAMETER*Math.PI);
-    public final static double TICKS_PER_DEGREE = TICK_MARKS*ROBOT_DIAMETER*Math.PI/360;
+    public final static double TICKS_PER_INCH = TICK_MARKS * CALLIBRATION / (WHEEL_DIAMETER * Math.PI);
+    public final static double TICKS_PER_DEGREE = TICK_MARKS * ROBOT_DIAMETER * Math.PI / 360;
 
     /**
-    This is the constructor that initializes the motors
-    */
+     * This is the constructor that initializes the motors
+     */
     public hexChassis_Andrew(LinearOpMode op) {
-        myOP = opMode;
+//        myOP = opMode;
         hardwareMap = op.hardwareMap;
 
         leftFront = hardwareMap.dcMotor.get("leftMotor");
         leftBack = hardwareMap.dcMotor.get("leftMotor");
         rightFront = hardwareMap.dcMotor.get("leftMotor");
         rightBack = hardwareMap.dcMotor.get("leftMotor");
-        
+
     }
 
     /**
      * Hex Motor Chassis Specific encoderDrive()
-     * @param distance Inches to move forward or backward (forward: +, backward: -)
+     *
+     * @param distance         Inches to move forward or backward (forward: +, backward: -)
      * @param opModeIsActiveas Type "opModeIsActive()" boolean in autonomousFrame (program extending LinerOpMode)
      */
-    public void encoderDriveFB(double distance, boolean opModeIsActiveas ) {
+    public void encoderDriveFB(double distance, boolean opModeIsActiveas) {
         int leftFrontTarget;
 
         leftFrontTarget = (int) Math.round(distance * TICKS_PER_INCH);
-        
+
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -101,24 +103,12 @@ public abstract class hexChassis_Andrew extends LinearOpMode {
         double motorLeftBackEncoder;
         double motorRightBackEncoder;
     }
-    public void inPlaceTurn(boolean right) {
-        if (boolean right == true) {
 
-        }
+    public void inPlaceTurn() {
 
     }
 
-
-    /**
-     * Hex Motor Chassis Specific encoderDrive()
-     * @param driveFB Inches to move forward or backward (forward: +, backward: -)
-     * @param speed Speed of robot (min: 0, max: 1)
-     */
-
-    public void encoderInPlaceTurn(double turnDegree, boolean turnDirection) {
-
-
-    }
+}
 
     /*
     public void startMotors() {
@@ -136,4 +126,4 @@ public abstract class hexChassis_Andrew extends LinearOpMode {
 
     }
     */
-}
+
