a9a1a56fb4ed218d41dbf17d4663dae178c70f40
redpandasforlife <aidenjma@gmail.com>
2019-11-11 19:40:36 -0800

    working with lift,claw, forward, backward, sideways left, sideways right, turn left, and turn right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/hexChassis_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/hexChassis_Teleop.java
@@ -84,11 +84,11 @@ public class hexChassis_Teleop extends LinearOpMode {
             if (motor_lift_up == 1.00) {
                 telemetry.addData("Motor", " RACK UP right_trigger (%.2f)", motor_lift_up);
                 telemetry.update();
-                if (!testing) robot.liftAutonomous(4);
+                if (!testing) robot.liftAutonomous(2);
             } else if (motor_lift_down == 1.00) {
                 telemetry.addData("Motor", " RACK DOWN left_trigger (%.2f)", motor_lift_down);
                 telemetry.update();
-                if (!testing) robot.liftAutonomous(-4);
+                if (!testing) robot.liftAutonomous(-2);
             } else if (stone_claw_up) {
                 telemetry.addData("Servo", " CLAW UP left_bumper");
                 telemetry.update();
