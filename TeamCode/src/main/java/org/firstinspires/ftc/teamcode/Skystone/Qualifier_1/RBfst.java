7071bf75fbf38bc37a222aedfd2788edb5e3c2fd
redpandasforlife <aidenjma@gmail.com>
2019-11-17 22:21:09 -0800

    it the reverse of BBfst and does not work for autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Qualifier_1/RBfst.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.Skystone.Qualifier_1;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Skystone.Qualifier_1.hexChassis;
+
+/**
+ * <h1>first SkyStone autonomous program</h1>
+ * for testing basicChassis that Nathan and Andrew wrote
+ * <p>
+ * Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Aiden Ma
+ * @version 2.0
+ * @since   2019-10-12
+ */
+@Autonomous(name = "RBfst")
+public class RBfst extends LinearOpMode {
+
+    private hexChassis robot   = new hexChassis();
+    private ElapsedTime  runtime = new ElapsedTime();
+    private Servo stone_claw_servo;
+
+    /**
+     * This method is for te autonomous operation of the robot on the Blue Alliance foundation side.
+     * 1st you inPlaceTurn left
+     * 2nd you go forward ~ 18 in.
+     * 3rd you turn right
+     * 4th you go forward ~ 24 in. to clamp the foundation
+     * 5th you back up with the foundation clamped
+     * 6th the unclamp the foundation
+     * 7th you inPlaceTurn left
+     * 8th move backward ~ 76 in.
+     * 9th inPlaceTurn 90 degrees right
+     * 10th move forward ~ 50 in.
+     * 11th inPlaceTurn 130 degrees left
+     * 12th move forward ~ 46 in.
+     * 13th inPlaceTurn 90 degrees left
+     * 8th you back up ~ 22 in. onto the blue line
+     */
+    @Override
+    public void runOpMode() {
+
+        stone_claw_servo = hardwareMap.servo.get("stone_claw_servo");
+
+        telemetry.addData("Status", "Ready to go"); telemetry.update();
+
+        robot.initChassis(this);
+
+        waitForStart();
+
+        robot.moveForward(39.5,0.55);
+        robot.inPlaceTurn(15,false,1);
+
+        stone_claw_servo.setPosition(-6);
+        sleep(1500);
+        stone_claw_servo.setPosition(-5);
+        sleep(1500);
+        robot.moveBackward(35,1);
+        stone_claw_servo.setPosition(7);
+        sleep(1500);
+        //robot.moveForward(4);
+        //robot.inPlaceTurn(145,false);
+        robot.moveLeft(15,.85);
+        robot.inPlaceTurn(30,true,1);
+        robot.moveLeft(15,.85);
+        robot.inPlaceTurn(30,true,1);
+        robot.moveLeft(10, .85);
+        robot.moveForward(10, 1);
+        robot.inPlaceTurn(90,true,1);
+        robot.moveForward(60,1);
+        robot.inPlaceTurn(70,false,1);
+        robot.moveForward(15,1);
+        stone_claw_servo.setPosition(-6);
+        sleep(1500);
+        robot.liftAutonomous(0.90);
+        robot.moveBackward(25,1);
+        robot.inPlaceTurn(80,false,1);
+        robot.moveForward(50,1);
+        robot.moveBackward(20,1);
+        stop();
+
+    }
+}
