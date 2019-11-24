ad2b3221c1b2be5a7467c3dc78c985e5dc349f8f
redpandasforlife <aidenjma@gmail.com>
2019-11-23 18:52:05 -0800

    works for autonomous is meh reliable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Qualifier_1/BBfst.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Qualifier_1/BBfst.java
@@ -53,25 +53,25 @@ public class BBfst extends LinearOpMode {
 
         waitForStart();
 
-        robot.moveForward(38,0.55);
+        robot.moveForward(37,0.55);
         robot.inPlaceTurn(100,true,1);
-        robot.moveForward(17,0.85);
+        robot.moveForward(20,0.85);
         robot.inPlaceTurn(100,false,1);
-        robot.moveForward(4,0.45);
-        robot.inPlaceTurn(25,true,1);
+        robot.moveForward(4.5,0.45);
+        robot.inPlaceTurn(25,false,1);
 
         stone_claw_servo.setPosition(-6);
         sleep(1000);
         stone_claw_servo.setPosition(-5);
         sleep(1000);
-        robot.moveBackward(35,1);
+        robot.moveBackward(40,1);
         stone_claw_servo.setPosition(7);
         sleep(200);
         //robot.moveForward(4);
         robot.moveBackward(3.5,0.20);
         robot.inPlaceTurn(112,false,.85);
-        robot.moveRight(15,0.85);
-        robot.moveForward(76,1);
+        robot.moveRight(12,0.85);
+        robot.moveForward(72,1);
         sleep(100);
         stone_claw_servo.setPosition(-5.5);
         sleep(100);
