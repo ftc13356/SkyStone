9516f48f40add8e4ab88b5bbaac970c32a4cd6fc
redpandasforlife <aidenjma@gmail.com>
2019-12-07 16:55:10 -0800

    changed the power controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Qualifier_1/hexChassis_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Qualifier_1/hexChassis_Teleop.java
@@ -81,31 +81,31 @@ public class hexChassis_Teleop extends LinearOpMode {
             if (y_button) {
                 motor_power = 1.0;
             }
-            if (left_stick_y == 1.00) {
+            if (left_stick_y > 0.1) {
                 telemetry.addData("Motor", " FORWARD left_y (%.2f)", left_stick_y);
                 telemetry.update();
-                if (!testing) robot.moveForwardTeleop(motor_power, 8);
-            } else if (left_stick_y == -1.00) {
+                if (!testing) robot.moveForwardTeleop(left_stick_y, 0);
+            } else if (left_stick_y < -0.1) {
                 telemetry.addData("Motor", " BACKWARD left_y(%.2f)", left_stick_y);
                 telemetry.update();
-                if (!testing) robot.moveBackwardTeleop(motor_power, 8);
-            } else if (left_stick_x == -1.00) {
+                if (!testing) robot.moveBackwardTeleop(-left_stick_y, 0);
+            } else if (left_stick_x < -0.1) {
                 telemetry.addData("Motor", " SIDEWAYS RIGHT left_x (%.2f)", left_stick_x);
                 telemetry.update();
-                if (!testing) robot.moveRightTeleop(motor_power, 5);
-            } else if (left_stick_x == 1.00) {
+                if (!testing) robot.moveRightTeleop(-left_stick_x, 0);
+            } else if (left_stick_x > 0.1) {
                 telemetry.addData("Motor", " SIDEWAYS LEFT left_x (%.2f)", left_stick_x);
                 telemetry.update();
-                if (!testing) robot.moveLeftTeleop(motor_power, 5);
-            } else if (right_stick_x == -1.00) {
+                if (!testing) robot.moveLeftTeleop(left_stick_x, 0);
+            } else if (right_stick_x < -0.1) {
                 telemetry.addData("Motor", " TURN RIGHT right_x (%.2f)", right_stick_x);
                 telemetry.update();
-                if (!testing) robot.inPlaceTurnTeleop(90, false, motor_power);
-            } else if (right_stick_x == 1.00) {
+                if (!testing) robot.inPlaceTurnTeleop(0, false, -right_stick_x);
+            } else if (right_stick_x > 0.1) {
                 telemetry.addData("Motor", " TURN LEFT right_x (%.2f)", right_stick_x);
                 telemetry.update();
-                if (!testing) robot.inPlaceTurnTeleop(90, true, motor_power);
-            } else if (left_stick_y == 0.00) {
+                if (!testing) robot.inPlaceTurnTeleop(0, true, right_stick_x);
+            } else {
                 telemetry.addData("STOP", " FORWARD left_y (%.2f)", left_stick_y);
                 telemetry.update();
                 if (!testing) robot.stopAllMotors();
@@ -132,22 +132,27 @@ public class hexChassis_Teleop extends LinearOpMode {
             if (lift_ground) {
                 telemetry.addData("Lift", " Lift goes to ground");
                 telemetry.update();
+                if (!testing) robot.stopAllMotors();
                 if (!testing) robot.liftPosition(0);
             } else if (lift_little) {
                 telemetry.addData("Lift", " Lift up slightly(1.2)");
                 telemetry.update();
+                if (!testing) robot.stopAllMotors();
                 if (!testing) robot.liftPosition(1.2);
             } else if (lift_level_1) {
                 telemetry.addData("Lift", " Lift goes to level 1 (5)");
                 telemetry.update();
+                if (!testing) robot.stopAllMotors();
                 if (!testing) robot.liftPosition(5);
             } else if (lift_level_2) {
                 telemetry.addData("Lift", " Lift goes to level 2 (8.25)");
                 telemetry.update();
+                if (!testing) robot.stopAllMotors();
                 if (!testing) robot.liftPosition(8.25);
             } else if (lift_level_3) {
                 telemetry.addData("Lift", " Lift goes to ground (11.75)");
                 telemetry.update();
+                if (!testing) robot.stopAllMotors();
                 if (!testing) robot.liftPosition(11.75);
             }
 
