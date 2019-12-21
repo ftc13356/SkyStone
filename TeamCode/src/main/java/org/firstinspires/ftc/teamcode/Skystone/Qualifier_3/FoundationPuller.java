31c6523b82f8fd4bfd310d32858026b7aadc2bee
redpandasforlife <aidenjma@gmail.com>
2019-12-21 09:26:40 -0800

    refined the position of the sticks so that it's pointing down

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Qualifier_3/FoundationPuller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Qualifier_3/FoundationPuller.java
@@ -27,7 +27,7 @@ public class FoundationPuller {
     //true = unclamp, false = clamp
     public void moveFoundationRightdown(boolean direction) {
         if (direction == true) {
-            foundationMoverRight.setPosition(0.5);
+            foundationMoverRight.setPosition(0.4);
         } else {
             foundationMoverRight.setPosition(0.0);
         }
@@ -44,7 +44,7 @@ public class FoundationPuller {
 */
     public void  moveFoundationLeftdown(boolean direction) {
         if (direction == true) {
-            foundationMoverLeft.setPosition(0.4);
+            foundationMoverLeft.setPosition(0.3);
         } else {
             foundationMoverLeft.setPosition(0.0);
         }
