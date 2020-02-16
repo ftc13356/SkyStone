3b8e3995e70a0b7382b8b437dfb5edd1329ba9d0
redpandasforlife <aidenjma@gmail.com>
2020-02-16 10:37:14 -0800

    works; tested 2/4 time worked

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Regional/B_stone1_Foundation_park_Reg.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Regional/B_stone1_Foundation_park_Reg.java
@@ -1,10 +1,11 @@
-package org.firstinspires.ftc.teamcode.Skystone.Qualifier_3;
+package org.firstinspires.ftc.teamcode.Skystone.Regional;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Skystone.Qualifier_3.RobotA;
 
 /**
  * <h1> SkyStone autonomous program</h1>
@@ -17,15 +18,15 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * @since 2020 Jan-7
  */
 
-@Disabled
-@Autonomous(name = "B_stone1_Foundation_park_Q3")
-public class B_stone1_Foundation_park_Q3 extends LinearOpMode {
+
+@Autonomous(name = "B_stone1_Foundation_park_Reg")
+public class B_stone1_Foundation_park_Reg extends LinearOpMode {
     RobotA robot = new RobotA();
     private ElapsedTime runtime = new ElapsedTime();
     private Servo stone_claw_servo;
 
 
-    public B_stone1_Foundation_park_Q3() {
+    public B_stone1_Foundation_park_Reg() {
 
     }
 
@@ -59,46 +60,35 @@ public class B_stone1_Foundation_park_Q3 extends LinearOpMode {
             a++;
             robot.moveBackwardIMU(0.2, 0.5);
         }
-        robot.moveForwardIMU(5,0.75);
-        robot.clawClamp(false);
-        robot.moveBackwardIMU(39,1.0);
-        robot.liftPosition(1.2);
-        robot.moveForwardIMU(1,0.2);
-        robot.fasterMoveLeftIMU(60-(a*8), 1.0);
-        robot.moveBackward(5,0.2);
-        sleep(1500);
-        robot.liftAutonomous(4.5);
-        sleep(1500);
-        robot.moveForwardIMU(20,1.0);
+        robot.moveForwardIMU(5,0.75);// torwards stone
+        robot.clawClamp(false);//gets stone
+        robot.moveBackwardIMU(16,1.0);
+        robot.AbsoluteTurnIMU(0,1.0); //auto-correct angle of robot
+        robot.liftPosition(1.2);//lifts lift a little bit up
+        robot.moveForwardIMU(0.4,0.2);
+        robot.fasterMoveLeftIMU(64-(a*8), 1.0);// crosses field
+        robot.liftPosition(4.4); // lifts lift to be able to place stone
+        robot.AbsoluteTurnIMU(0,1.0);////auto-correct angle of robot
+        robot.fasterMoveLeftIMU(48,1.0);//lines up with foundation
         //robot.moveLeftIMU(10,0.6);
-        robot.moveForwardIMU(3,0.2 );
+        robot.moveForwardIMU(6.15,0.2 );
+        robot.liftPosition(2.3);
+        robot.clawClamp(true);
         robot.moveFoundationLefttdown(true);
         robot.moveFoundationRightdown(true);
-        robot.moveBackwardIMU(30,1.0);
+        robot.moveBackwardIMU(33,1.0);
         robot.moveFoundationLefttdown(false);
         robot.moveFoundationRightdown(false);
-        robot.moveBackwardIMU(7,0.2);
+        robot.moveBackwardIMU(2,0.2);
+        robot.AbsoluteTurnIMU(0,1.0);
         //robot.AbsoluteTurnIMU(90,0.6);
+        robot.fasterMoveRightIMU(38.5,1.0);
+        robot.moveForward(20,1.0);
         robot.liftPosition(0);
-        robot.moveRightUntilBlue();
-        robot.clawClampPosition(0.0);
-        //robot.moveBackwardIMU(5,0.2);
-        //robot.liftPosition(2.83.0);
-        //robot.fasterMoveLeftIMU(58, 1.0);
-        /*robot.AbsoluteTurnIMU(0,1.0);
-        robot.moveForwardIMU(32,1);
-        robot.moveLeftIMU(10,0.6);
-        robot.moveForwardIMU(5,0.2 );
-        robot.moveFoundationLefttdown(true);
-        robot.moveFoundationRightdown(true);
-        robot.moveBackwardIMU(35,1.0);
-        robot.moveFoundationLefttdown(false);
-        robot.moveFoundationRightdown(false);
-        robot.moveBackwardIMU(7,0.2);
-        robot.AbsoluteTurnIMU(90,0.6);
-        robot.liftPosition(0);
-        robot.moveBackwardUntilBlue();
-        robot.clawClampPosition(0.0);*/
+        robot.AbsoluteTurnIMU(0,1.0);
+        robot.fasterMoveRightIMU(30,1.0);
+ //     robot.moveRightUntilBlue();
+
 
     }
 }
