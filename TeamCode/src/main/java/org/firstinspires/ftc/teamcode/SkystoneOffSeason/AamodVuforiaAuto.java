029314b5c14028bf50f8300225d64614775e5bc4
awesomeaamod <awesome.aamod@gmail.com>
2020-04-04 23:02:07 -0700

    I made a program that can detect the VuMark Navigation Targets and output where the robot is.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneOffSeason/AamodVuforiaAuto.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.SkystoneOffSeason;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "AamodVuforiaAuto")
+public class AamodVuforiaAuto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        AamodVuforia vuforia = new AamodVuforia(this);
+
+        waitForStart();
+        vuforia.start();
+
+        while (opModeIsActive()) {
+            telemetry.addData("X", vuforia.getVuforiaX());
+            telemetry.addData("Y", vuforia.getVuforiaY());
+            telemetry.addData("Angle", vuforia.getVuforiaAngle());
+            telemetry.addData("Target", vuforia.getVuforiaTrackable());
+            telemetry.update();
+        }
+        vuforia.interrupt();
+    }
+}
