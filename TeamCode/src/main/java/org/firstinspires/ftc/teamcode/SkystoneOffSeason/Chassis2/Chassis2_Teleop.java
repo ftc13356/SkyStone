e06b28aa0632ddf495cdae22ca24e436d01bdf39
Nathan <nathanko2007@gmail.com>
2020-08-25 17:32:47 -0700

    Teleop now works - tested and works perfectly, but i just need to add the button modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneOffSeason/Chassis2/Chassis2_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneOffSeason/Chassis2/Chassis2_Teleop.java
@@ -1,28 +1,31 @@
 package org.firstinspires.ftc.teamcode.SkystoneOffSeason.Chassis2;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "Chassis2_Teleop ")
 public class Chassis2_Teleop extends LinearOpMode {
     private Robot robot = new Robot();
-    double power = .3;
-    double angle = 0;
     private DcMotor motorLeftFront;
     private DcMotor motorLeftBack;
     private DcMotor motorRightFront;
     private DcMotor motorRightBack;
-    float left_stick_y = -gamepad1.left_stick_y;
-    float left_stick_x = -gamepad1.left_stick_x;
-    float right_stick_x = -gamepad1.right_stick_x;
-    double magnitude = Math.sqrt(Math.pow(left_stick_x, 2) + Math.sqrt(Math.pow(left_stick_y, 2)));
-    double inverseTangent = Math.atan2(left_stick_y, left_stick_x);
+    float left_stick_y;
+    float left_stick_x;
+    float right_stick_x;
 
+    @Override
     public void runOpMode() {
+
+        double magnitude;
+        double angleInRadian;
+        double angleInDegree;
+
+        motorLeftFront = hardwareMap.dcMotor.get("motorLeftFront");
+        motorLeftBack = hardwareMap.dcMotor.get("motorLeftBack");
+        motorRightFront = hardwareMap.dcMotor.get("motorRightFront");
+        motorRightBack = hardwareMap.dcMotor.get("motorRightBack");
 
         telemetry.addData("Status", "Ready to go");
         telemetry.update();
@@ -36,26 +39,46 @@ public class Chassis2_Teleop extends LinearOpMode {
 
         while (!isStopRequested()) {
 
+            float left_stick_y = -gamepad1.left_stick_y;
+            float left_stick_x = -gamepad1.left_stick_x;
+
+            magnitude = Math.sqrt(Math.pow(left_stick_x, 2) + Math.sqrt(Math.pow(left_stick_y, 2)));
+            angleInRadian = Math.atan2(left_stick_y, left_stick_x);
+            angleInDegree = Math.toDegrees(angleInRadian);
+
+
             telemetry.addData("Motor", " idk)", magnitude);
             telemetry.update();
-            multidirectionalMove(magnitude);
+            multidirectionalMove(magnitude, angleInDegree);
 
         }
             idle();
     }
-        public void multidirectionalMove(double power) {
-            motorLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            motorRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            motorLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            motorRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    
-            motorLeftBack.setPower(Math.sin(inverseTangent - Math.PI/4) * magnitude + right_stick_x);
-            motorRightBack.setPower(Math.sin(inverseTangent + Math.PI/4) * magnitude + right_stick_x);
-            motorLeftFront.setPower(Math.sin(inverseTangent + Math.PI/4) * magnitude + right_stick_x);
-            motorRightFront.setPower(Math.sin(inverseTangent - Math.PI/4) * magnitude + right_stick_x);
-    
-        }
 
+    /**
+     * moves in any direction
+     * @param   power   from values -1 through 1
+     * @param   angle   angle in degrees
+     * @return  void
+     */
+    public void multidirectionalMove(double power, double angle) {
+
+        float right_stick_x = -gamepad1.right_stick_x;
+
+        double angleInRadian;
+        angleInRadian = Math.toRadians(angle);
+
+        motorLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorLeftBack.setPower(Math.sin(angleInRadian - Math.PI/4) * power - right_stick_x);
+        motorRightBack.setPower(Math.sin(angleInRadian + Math.PI/4) * power + right_stick_x);
+        motorLeftFront.setPower(Math.sin(angleInRadian + Math.PI/4) * power - right_stick_x);
+        motorRightFront.setPower(Math.sin(angleInRadian - Math.PI/4) * power + right_stick_x);
+
+    }
 
 }
 
